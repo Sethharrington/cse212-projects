c8af26aa56d84a5f5e43fdbd5f6c17533eff8cf4
Seth Harrington <sbgha.09@gmail.com>
2025-07-10 08:14:14 -0600

    Code activities made, completing Arrays activities

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +16,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        /// Create an array of capacity "length"
+        /// Make a for from 1 to "length" (i = 0; i < length, i++)
+        /// Add the multiplication of i * number
+        /// Return de Array
+
+        var multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = (i + 1) * number;
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,25 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        /// Check that amount and the list data.count are different, otherwise don't do anything
+        /// If amount and data.count are different
+        /// Create 2 list
+        /// In the first list save the first part of the array, from 0 to the data.count - amount
+        /// In the second list save the rest of the list, from data.count -amount to data.count
+        /// Clear the data list
+        /// Insert the first list into data
+        /// Insert the second list into data
+        if (amount != data.Count)
+        {
+            List<int> d1 = data.GetRange(0, data.Count - amount);
+            List<int> d2 = data.GetRange(data.Count - amount, amount);
+            data.Clear();
+            data.InsertRange(0, d1);
+            data.InsertRange(0, d2);
+        }
+
+        // data.RemoveRange(amount, data.Count - amount);
+        // data.InsertRange(0, d1);
     }
 }
